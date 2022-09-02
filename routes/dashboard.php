501dd9ac812ef3cf27ed3ea89ce71cfc077bba01
Rajesh Dewle <rajesh_dewle@hotmail.com>
2022-09-02 11:16:10 +0530

    Add custom route file and declare dashboard routes

---
+++ routes/dashboard.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Dashboard\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Dashboard Routes
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::get('/dashboard', [PostController::class, 'index'])->name('dashboard');
+Route::get('/dashboard/create-post', [PostController::class, 'create'])->name('post.create');
+Route::post('/dashboard/create-post', [PostController::class, 'store'])->name('post.store');
+Route::get('/dashboard/sorted-post', [PostController::class, 'sorted'])->name('post.sorted');

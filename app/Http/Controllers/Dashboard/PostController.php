ec75eaf250e2168201f97f0f724fc83a55a0ef86
Rajesh Dewle <rajesh_dewle@hotmail.com>
2022-09-02 11:21:07 +0530

    Add post management in dashboard

---
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $sort = 'desc';
+        if ($request->query('sort') == 'asc') {
+            $sort = $request->query('sort');
+        }
+
+        return Inertia::render('Dashboard', [
+            'posts' => auth()->user()->posts()->orderBy('published_at', $sort)->simplePaginate()->appends('sort', $sort),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Posts/CreatePost');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\StorePostRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePostRequest $request)
+    {
+        $request->user()->posts()->create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'published_at' => now(),
+        ]);
+
+        return redirect('dashboard');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}

12869548298f4d9a903ae29c2cda3f131d578bb0
Rajesh Dewle <rajesh_dewle@hotmail.com>
2022-09-02 11:29:07 +0530

    Add no post component for dashbaord

---
+++ resources/js/Components/NoPosts.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-4xl text-gray-700 text-center py-8">
+        <img src="/images/no-post.svg" alt="No Post" width="200" class="mx-auto py-8">
+        There are no posts added yet!
+    </div>
+</template>

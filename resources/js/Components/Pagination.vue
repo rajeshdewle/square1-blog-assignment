4f6c81b8d903d9d1208ae1502ceafe2a16385d5c
Rajesh Dewle <rajesh_dewle@hotmail.com>
2022-09-02 11:30:15 +0530

    Add pagination component for posts

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3"
+
+defineProps({
+    pages: {
+        type: Object,
+    }
+})
+</script>
+
+<template>
+    <div class="flex justify-between">
+        <Link
+            v-if="pages.prev_page_url"
+            :href="pages.prev_page_url"
+            class="rounded-lg py-2 px-12 border text-sm text-gray-600 hover:text-gray-900"
+        >
+            &larr; Previous
+        </Link>
+        <button
+            v-else
+            disabled
+            class="cursor-not-allowed text-sm rounded-lg py-2 px-12 text-gray-500 border"
+        >
+            &larr; Previous
+        </button>
+        <Link
+            v-if="pages.next_page_url"
+            :href="pages.next_page_url"
+            class="rounded-lg py-2 px-12 border text-sm text-gray-600 hover:text-gray-900"
+        >
+            Next &rarr;
+        </Link>
+        <button
+            v-else
+            disabled
+            class="cursor-not-allowed text-sm rounded-lg py-2 px-12 text-gray-500 border"
+        >
+            Next &rarr;
+        </button>
+    </div>
+</template>

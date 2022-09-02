6d24e725db6bd27a9572fb915578cd1bf940c475
Rajesh Dewle <rajesh_dewle@hotmail.com>
2022-09-02 13:22:47 +0530

    Add dashboard.js in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/dashboard.js",
             ],
             refresh: true,
         }),
